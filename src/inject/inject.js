9526079cd4629b8be427bcf120a129e978e11f01
Jason Goldstein <jason@betheshoe.com>
2017-03-16 10:37:12 -0400

    Step 1: 16px body font, so I don't go blind.

---
+++ src/inject/inject.js
@@ -0,0 +1,13 @@
+chrome.extension.sendMessage({}, function(response) {
+	var readyStateCheckInterval = setInterval(function() {
+	if (document.readyState === "complete") {
+		clearInterval(readyStateCheckInterval);
+
+		// ----------------------------------------------------------
+		// This part of the script triggers when page is done loading
+		console.log("TODO: Markdown and custom shortcuts?");
+		// ----------------------------------------------------------
+
+	}
+	}, 10);
+});
